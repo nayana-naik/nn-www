67ff822ece693fbac6d7802c7f8905e9118aebe0
nayana-naik <47020002+nayana-naik@users.noreply.github.com>
2020-06-22 17:28:56 -0400

    removed port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express =  require ('express')
 const app = express()
-const port = 3000
+
 
 
 const env2=process.env.NODE_OPTIONS
@@ -9,9 +9,6 @@ app.get('/', (req,res)=>{
     res.send('Hello World')
 })
 
-app.listen(port, ()=>{
-    console.log(`Listening to port ${port}!`)
-})
 app.get('/hello',(req,res)=>{
     res.send(env2)
 })
