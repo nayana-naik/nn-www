53fe71660f6325249ff05360af219ef3e5d31fba
nayana-naik <47020002+nayana-naik@users.noreply.github.com>
2020-06-22 17:38:02 -0400

    fghjk

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express =  require ('express')
 const app = express()
 
+const server = http.createServer((request, response) => {
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("Hello World!");
+});
 
+const port = process.env.PORT || 1337;
+server.listen(port);
 
 const env2=process.env.NODE_OPTIONS
 
